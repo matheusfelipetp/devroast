1c3afdc19ac53c32a7a8f98f297a74b8a16d3e60
matheusfelipetp <matheusfelipetp@outlook.com>
2026-04-03 16:12:32 -0300

    fix: remove getStats prefetch so NumberFlow animates from zero to real value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 import { RoastStats } from "@/components/roast-stats";
-import { HydrateClient, prefetch, trpc } from "@/trpc/server";
 import { HomeContent } from "./home-content";
 
 export default function Home() {
-	prefetch(trpc.roast.getStats.queryOptions());
-
-	return (
-		<HydrateClient>
-			<HomeContent stats={<RoastStats />} />
-		</HydrateClient>
-	);
+	return <HomeContent stats={<RoastStats />} />;
 }
